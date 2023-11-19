726135e0070c523db4c57640b637a0596fee822d
pizzaolive <git_projects@protonmail.com>
2023-11-19 15:32:36 +0000

    Add raw string and comments

--- cover_art_uploader/parameters.py
+++ cover_art_uploader/parameters.py
@@ -1,4 +1,4 @@
 IMGBB_API_KEY = "XXXXXXXXXXXXXXX"
-INPUT_FOLDER = "C:\Users\User\Music"
-OUTPUT_FOLDER = "C:\Users\User\Music"
+INPUT_FOLDER = r"C:\Users\User\Music" # Replace the values inside the quote marks
+OUTPUT_FOLDER = r"C:\Users\User\Music" # Ensure there is no backslash at the end of the string
 OUTPUT_IMAGE_SIZE = (750, 750)
